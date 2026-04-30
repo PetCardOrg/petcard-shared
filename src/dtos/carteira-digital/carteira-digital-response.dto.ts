00e7b76b383878b49aecc52530c267c611826cc9
Camila <camilampinheiro1108@gmail.com>
2026-04-30 15:10:22 -0300

    feat(carteira): adiciona contratos da carteira digital para consumo no mobile PC-051

--- src/dtos/carteira-digital/carteira-digital-response.dto.ts
+++ src/dtos/carteira-digital/carteira-digital-response.dto.ts
@@ -8,12 +8,16 @@ export class CarteiraDigitalResponseDto {
   breed?: string;
   sex!: Sex;
   birth_date?: string;
+  weight?: number;
   photo_url?: string;
   qr_code_url?: string;
   tutor_id!: string;
   tutor_name!: string;
   vaccines_count!: number;
+  upcoming_vaccines_count!: number;
   dewormings_count!: number;
+  upcoming_dewormings_count!: number;
   medications_count!: number;
+  active_medications_count!: number;
   issued_at!: Date;
 }
